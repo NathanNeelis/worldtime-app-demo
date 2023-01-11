71802464821092ce9b14169bffebefc433dbce4e
Nathan Neelis <nathan.neelis@wearetriple.com>
2023-01-11 19:36:32 +0100

    commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  String time = 'loading';
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Amsterdam', flag: 'netherlands.png', url: 'Europe/Amsterdam');
+    await instance.getTime();
+
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime,
+    });
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SpinKitRipple( // flutter package SpinKit
+          color: Colors.grey,
+          size: 80.0,
+        ),
+      ),
+    );
+  }
+}
